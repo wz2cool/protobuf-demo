418f10c3eee4fc6e0e132c57f7c45939e541a170
wz2cool <wz2cool@live.cn>
2018-08-02 10:29:03 +0800

    add

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     var inputGet = $("#input_get");
+    var inputRawGet = $("#input_raw_get");
     inputGet.click(getMessage);
-
+    inputRawGet.click(getRawMessage);
 
     function getMessage() {
         $.get("test/message", function (data, err) {
@@ -19,10 +20,28 @@ $(document).ready(function () {
                 console.log("result: " + JSON.stringify(result));
             });
         });
-
-
     }
 
+    function getRawMessage() {
+        var url = "https://raw.githubusercontent.com/wz2cool/fake-data/master/protobuff/price/price1.txt";
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.responseType = 'arraybuffer';
+        xhr.onload = function (ev) {
+            var responseArray = new Uint8Array(this.response);
+            protobuf.load("proto/best_price.proto", function (err, root) {
+                if (err) {
+                    console.error(err);
+                    throw err;
+                }
+
+                var message = root.lookupType("BestPriceList");
+                var result = message.decode(responseArray);
+                console.log("result: " + JSON.stringify(result));
+            });
+        };
+        xhr.send();
+    }
 
 });
 
